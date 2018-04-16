b722e0fafe2edb564bbef50ea71b538b7822085c
Damian West <damian@damian.id.au>
2018-04-17 09:35:54 +1000

    Updated to cater for png vs png24 files from 2017 on.

--- download_submarine_cable_map.sh
+++ download_submarine_cable_map.sh
@@ -9,12 +9,22 @@ if [ -z ${1} ];
 fi
 
 # build the URL
-URL_BASE="http://c.tiles.telegeography.com/maps/submarine-cable-map-$YEAR/6/"
+URL_BASE="http://c.tiles.telegeography.com/maps/submarine-cable-map-$YEAR/6"
+
+# build the file suffix
+if [ $1 > 2016 ];
+  then
+    SUF=png24
+  else
+    SUF=png
+fi
+
+echo "Starting download of $YEAR map from TeleGeography..."
 
 # loop through the 64 frames (0-63)
 for x in `seq 0 1 63`; do
   for y in `seq 0 1 63`; do
-    URL=${URL_BASE}/${x}/${y}.png
+    URL=${URL_BASE}/${x}/${y}.$SUF
     wget -q $URL -O `printf "%08d_%08d.png" ${y} ${x}`
     echo -n .
   done
@@ -22,5 +32,4 @@ for x in `seq 0 1 63`; do
   echo
 done
 
-# build the final png
 montage -mode Concatenate -tile x64 *_*.png sub_$YEAR.png
